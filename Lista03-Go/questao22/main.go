30f3a16abf51d0d48366d9b87f823d7f68a6d177
claudioferreira23 <166179154+claudioferreira23@users.noreply.github.com>
2024-05-17 16:08:15 -0300

    Add files via upload

--- Lista03-Go/questao22/main.go
+++ Lista03-Go/questao22/main.go
@@ -1,9 +1,51 @@
 package main
 
-import(
+import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func maiorPremio(numero string, n, d int) string {
+	pilha := make([]byte, 0, d)
+	remover := n - d
+
+	for i := 0; i < n; i++ {
+		for len(pilha) > 0 && remover > 0 && pilha[len(pilha)-1] < numero[i] {
+			pilha = pilha[:len(pilha)-1]
+			remover--
+		}
+		pilha = append(pilha, numero[i])
+	}
+
+	return string(pilha[:d])
+}
+
 func main() {
-	
+	leitor := bufio.NewReader(os.Stdin)
+	var resultados []string
+
+	for {
+		entrada, _ := leitor.ReadString('\n')
+		entrada = strings.TrimSpace(entrada)
+		if entrada == "0 0" {
+			break
+		}
+
+		partes := strings.Split(entrada, " ")
+		n, _ := strconv.Atoi(partes[0])
+		d, _ := strconv.Atoi(partes[1])
+
+		numero, _ := leitor.ReadString('\n')
+		numero = strings.TrimSpace(numero)
+
+		resultado := maiorPremio(numero, n, d)
+		resultados = append(resultados, resultado)
+	}
+
+	for _, resultado := range resultados {
+		fmt.Println(resultado)
+	}
 }
--- Lista03-Go/questao23/main.go
+++ Lista03-Go/questao23/main.go
@@ -1,9 +1,61 @@
 package main
 
-import(
+import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
+	"strings"
 )
 
-func main(){
-	
+func freqvogal(s string) [5]int {
+	var freq [5]int
+	for _, caracter := range strings.ToLower(s) {
+		switch caracter {
+		case 'a':
+			freq[0]++
+		case 'e':
+			freq[1]++
+		case 'i':
+			freq[2]++
+		case 'o':
+			freq[3]++
+		case 'u':
+			freq[4]++
+		}
+	}
+	return freq
+}
+
+func diseuclid(v1, v2 [5]int) float64 {
+	var soma float64
+	for i := 0; i < 5; i++ {
+		dif := v1[i] - v2[i]
+		soma += float64(dif * dif)
+	}
+	return math.Sqrt(soma)
+}
+
+func main() {
+	leitor := bufio.NewReader(os.Stdin)
+	entrada, _ := leitor.ReadString('\n')
+	entrada = strings.TrimSpace(entrada)
+
+	partes := strings.Split(entrada, ";")
+	if len(partes) != 2 {
+		fmt.Println("FORMATO INVALIDO!")
+		return
+	}
+
+	A := strings.TrimSpace(partes[0])
+	B := strings.TrimSpace(partes[1])
+
+	freqA := freqvogal(A)
+	freqB := freqvogal(B)
+
+	fmt.Printf("(%d,%d,%d,%d,%d)\n", freqA[0], freqA[1], freqA[2], freqA[3], freqA[4])
+	fmt.Printf("(%d,%d,%d,%d,%d)\n", freqB[0], freqB[1], freqB[2], freqB[3], freqB[4])
+
+	distance := diseuclid(freqA, freqB)
+	fmt.Printf("%.2f\n", distance)
 }
